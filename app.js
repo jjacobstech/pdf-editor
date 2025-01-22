50183abc1d0a1938d106e5acf13096406d7b0b34
jjacobstech <jacobsjoshua81@gmail.com>
2025-01-22 10:28:54 +0100

    Update

--- app.js
+++ app.js
@@ -1,28 +1,16 @@
-import http from "http"
+import "./assets/pspdfkit.js";
 
-const port = 5000;
+// We need to inform PSPDFKit where to look for its library assets, i.e. the location of the `pspdfkit-lib` directory.
+const baseUrl = `${window.location.protocol}//${window.location.host}/assets/`;
 
-const app = http.createServer((req, res) => {
-
+PSPDFKit.load({
+    baseUrl,
+    container: "#pspdfkit",
+    document: "document.pdf"
 })
-// import "./assets/pspdfkit.js";
-
-// // We need to inform PSPDFKit where to look for its library assets, i.e. the location of the `pspdfkit-lib` directory.
-// const baseUrl = `${window.location.protocol}//${window.location.host}/assets/`;
-
-// PSPDFKit.load({
-//     baseUrl,
-//     container: "#pspdfkit",
-//     document: "document.pdf"
-// })
-//     .then(instance => {
-//         console.log("PSPDFKit loaded", instance);
-//     })
-//     .catch(error => {
-//         console.error(error.message);
-//     });
-app.listen(() => {
-    console.log('====================================');
-    console.log(`Server Started at localhost:${port}`);
-    console.log('====================================');
-})
+    .then(instance => {
+        console.log("PSPDFKit loaded", instance);
+    })
+    .catch(error => {
+        console.error(error.message);
+    });
